fcff8a1eafba60243339c933bfb7363fac5d4223
mikan <santanud5d80@gmail.com>
2025-07-19 21:26:48 +0530

    logos added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,12 @@ import PortfolioPreview from '../components/PortfolioPreview';
 import About from '../components/About';
 import Testimonials from '../components/Testimonials';
 import CallToAction from '../components/CallToAction';
-
+import ClientLogos from '../components/ClientLogos';
 const Home: React.FC = () => {
   return (
     <div className="bg-surface-tertiary">
       <Hero />
+      <ClientLogos />
       <CompanyMarquee />
       <ServicesPreview />
       <PortfolioPreview />
